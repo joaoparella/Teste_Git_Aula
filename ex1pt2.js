c3a8175a84df91dfbad9a0bb49a97354c9003438
joao <joao.pparella@senacsp.edu.br>
2025-01-28 19:57:33 -0300

    alteração no arquivo do exercício 1 - alterado a descricação de metros, para W

--- ex1pt2.js
+++ ex1pt2.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
-var metros = Number(prompt("Digite aqui o numero de metros: "));
+var metros = Number(prompt("Digite aqui o numero de m:"));
 
 var convertido = metros / 1000
 
