431e8f8bfe313abed05b1e5dec95dc0bc1e19358
joao <joao.pparella@senacsp.edu.br>
2025-01-28 21:24:57 -0300

    Testando a criação de um arquivo e correção da mensagem

--- ex1pt4.js
+++ ex1pt4.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
-let numero = Number(prompt("Digite um numero"))
+let numero = Number(prompt("Digite um numero para saber se é par ou impar"))
 
 if(numero % 2 == 0){
     console.log("Par")
